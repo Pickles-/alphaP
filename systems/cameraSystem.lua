9db103cd051572c36dd4d94c78f0b3654fd74fbe
Devon Lambert <devmlambert@gmail.com>
2019-11-09 09:01:25 -0800

    Updated Camera to follow playera again

--- systems/cameraSystem.lua
+++ systems/cameraSystem.lua
@@ -1,6 +1,6 @@
 local cameraSystem = tiny.processingSystem(class "CameraSystem")
 
-cameraSystem.filter = tiny.requireAll("position", "isPlayer")
+cameraSystem.filter = tiny.requireAll("collider", "isPlayer")
 
 function cameraSystem:init(camera)
     self.camera = camera
@@ -8,7 +8,7 @@ end
 
 function cameraSystem:process(e, dt)
    local  width, height = love.graphics.getDimensions()
-   self.camera:setPosition(e.position.x - width / 2, e.position.y - height / 2)
+   self.camera:setPosition(e.collider.x - width / 2, e.collider.y - height / 2)
 end
 
 return cameraSystem
